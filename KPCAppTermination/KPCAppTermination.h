880d5affe36ecf401aa89e1f3b938db839f0f887
Cédric <cedric@onekilopars.ec>
2016-08-07 13:27:59 +0200

    Initial Commit

---
+++ KPCAppTermination/KPCAppTermination.h
@@ -0,0 +1,19 @@
+//
+//  KPCAppTermination.h
+//  KPCAppTermination
+//
+//  Created by Cédric Foellmi on 07/08/16.
+//  Copyright © 2016 onekiloparsec. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KPCAppTermination.
+FOUNDATION_EXPORT double KPCAppTerminationVersionNumber;
+
+//! Project version string for KPCAppTermination.
+FOUNDATION_EXPORT const unsigned char KPCAppTerminationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KPCAppTermination/PublicHeader.h>
+
+
